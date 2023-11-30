341cbebdde5e930876bc87bef9f8ed2f65ba3940
sergito <yozic98@gmail.com>
2023-11-30 22:19:03 +0300

    commented

--- extract.sh
+++ extract.sh
@@ -3,4 +3,4 @@
 ffpb -i "$1" -bsf:a aac_adtstoasc -vcodec copy -c copy -preset ultrafast _$2.mp4
 ffpb -i _$2.mp4 -vcodec libx265 -preset ultrafast $2.mp4
 rm _$2.mp4 
-mv $2.mp4 /mnt/c/Users/sergito/Documents/reduced
+# mv $2.mp4 /mnt/c/Users/sergito/Documents/reduced
